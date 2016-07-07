17dd4cf4bacc25773a96ff3fe6b88925001ecf8a
jgoley <jgoley@gmail.com>
2016-07-07 10:57:23 -0400

    Fix script to run local parse server - use from GoodQuestion repo

--- app/.scripts/parse-server-local.js
+++ app/.scripts/parse-server-local.js
@@ -1,32 +1,38 @@
 #!/usr/bin/env node
 
-// Note: command to start parse-dashboard server is:
-//       parse-dashboard --appId Acceptance --masterKey test --serverURL "http://localhost:31337/parse" --appName Acceptance\ Tests
+'use strict'
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
+var Settings = require('../settings-dev.json')
 
-var port = 31337;
+var port = 1337
+
+var express = require('express')
+var ParseServer = require('parse-server').ParseServer
 
 console.log('Initiating local parse server instance on port ' + port)
 
 var app = express();
 var api = new ParseServer({
   databaseURI: 'mongodb://localhost:13001/meteor',
-  appId: 'Acceptance',
-  masterKey: 'test',
-  serverURL: 'http://localhost:' + port + '/parse'
-});
+  appId: Settings.public.parseAppId,
+  masterKey: Settings.private.parse.masterKey,
+  serverURL: Settings.public.parseServerUrl
+})
 
 // Serve the Parse API at /parse URL prefix
-app.use('/parse', api);
+app.use('/parse', api)
 
 // CORS
 app.use(function(mountPath, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  return next();
-});
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  return next()
+})
 
 app.listen(port, function() {
-  console.log('parse-server-example running on port ' + port + '.');
-});
+  console.log('Local parse server instance is running on port ' + port)
+  console.log('\nThe command to start parse-dashboard:')
+  console.log('parse-dashboard --appId ' + Settings.public.parseAppId + ' \\')
+  console.log('                --masterKey "' + Settings.private.parse.masterKey + '" \\')
+  console.log('                --serverURL "' + Settings.public.parseServerUrl + '" \\')
+  console.log('                --appName TestApp')
+})
